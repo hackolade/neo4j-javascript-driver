56ff568cfb4ddcfff7561662d5849bda36cca7d5
Taras Dubyk <taras.dubyk@binary-studio.com>
2020-03-11 11:36:26 +0200

    add server-client ssl strategy

--- src/internal/channel-config.js
+++ src/internal/channel-config.js
@@ -36,7 +36,8 @@ const ALLOWED_VALUES_TRUST = [
   undefined,
   'TRUST_ALL_CERTIFICATES',
   'TRUST_CUSTOM_CA_SIGNED_CERTIFICATES',
-  'TRUST_SYSTEM_CA_SIGNED_CERTIFICATES'
+  'TRUST_SYSTEM_CA_SIGNED_CERTIFICATES',
+  'TRUST_SERVER_CLIENT_CERTIFICATES'
 ]
 
 export default class ChannelConfig {
@@ -54,6 +55,9 @@ export default class ChannelConfig {
     this.knownHostsPath = extractKnownHostsPath(driverConfig)
     this.connectionErrorCode = connectionErrorCode || SERVICE_UNAVAILABLE
     this.connectionTimeout = extractConnectionTimeout(driverConfig)
+    this.key = extractClientKey(driverConfig)
+    this.cert = extractClientCertificates(driverConfig)
+    this.passphrase = extractClientCertificatePassphrase(driverConfig)
   }
 }
 
@@ -100,4 +104,16 @@ function extractConnectionTimeout (driverConfig) {
     // timeout configured, use the provided value
     return configuredTimeout
   }
+}
+
+function extractClientKey (driverConfig) {
+  return driverConfig.key || ''
+}
+
+function extractClientCertificates (driverConfig) {
+  return driverConfig.cert || ''
+}
+
+function extractClientCertificatePassphrase (driverConfig) {
+  return driverConfig.passphrase || ''
 }
